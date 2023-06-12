c949d3602929fda9ed52175463b3efaef1fa7af2
Motblunt <theo.cottin@gmail.com>
2023-06-12 09:41:10 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const booksRoutes = require('./routes/books');
+const userRoutes = require('./routes/user');
+const path = require('path');
+const dotenv = require('dotenv')
+dotenv.config();
+const mongoDBMDP = process.env.MONGODB_MDP
+
+
+// Connexion à la base de données
+mongoose.connect(`mongodb+srv://theocottin:${mongoDBMDP}@monvieuxgrimoire.92fc45b.mongodb.net/?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Création de l'application
+const app = express();
+// Middleware permettant à Express d'extraire le corps JSON des requêtes POST
+app.use(express.json());
+
+// Middleware gérant les erreurs de CORS
+app.use((req, res, next) => {
+    // Accès à notre API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Autorisation d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // Autorisation d'envoyer des requêtes avec les méthodes mentionnées
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Gestion de la ressource images de manière statique
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// Enregistrement des routeurs
+app.use('/api/auth', userRoutes);
+app.use('/api/books', booksRoutes);
+
+module.exports = app;
